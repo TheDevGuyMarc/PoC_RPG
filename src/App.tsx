8b59fa67add70136b11b1c5255d914f0dadf4f82
Marc Hannappel <thedevguymarc@gmail.com>
2023-10-23 09:19:12 +0200

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Game from "./views/GameComponent";
 import GameComponent from "./views/GameComponent";
 
 function App() {
